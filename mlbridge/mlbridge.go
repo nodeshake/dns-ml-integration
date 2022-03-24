57fb113ba5c7070afe2a35ffc92d9eb6234c7fe3
nodeshake <150516242+nodeshake@users.noreply.github.com>
2022-03-24 20:45:45 +0800

    basic project structure

--- mlbridge/mlbridge.go
+++ mlbridge/mlbridge.go
@@ -28,4 +28,5 @@ func (p Mlbridge) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg
 	jsonData := map[string]string{"Domain Name": qname, "IP": ip}
     jsonValue, _ := json.Marshal(jsonData)
     response, err := http.Post("http://127.0.0.1:5000/", "application/json", bytes.NewBuffer(jsonValue))
-    if err != nil
+    if err != nil {
+  
