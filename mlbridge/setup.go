2543e5b95144b8bf15659a7073fa36732dc4ecc7
nodeshake <150516242+nodeshake@users.noreply.github.com>
2023-07-28 22:58:38 +0800

    Update hadoop.md

--- mlbridge/setup.go
+++ mlbridge/setup.go
@@ -6,3 +6,9 @@ import (
 	"github.com/coredns/coredns/plugin"
 	"github.com/caddyserver/caddy"
 )
+
+func init() {
+	caddy.RegisterPlugin("mlbridge", caddy.Plugin{
+		ServerType: "dns",
+		Action:     setup,
+	})
